b65cceba8c48bf433160d0e9d42e981b8d89e76a
nurbaitenov <baitenovnur@gmail.com>
2026-07-16 17:58:04 +0000

    added

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ sudo systemctl enable docker
 systemctl status docker 
 
 
+# this code is for when you what to attach worker notes to Docker Swarm manager notes 
+# docker swarm join --token SWMTKN-1-3ntfrsun6n33du59fsf1tjqhvwp67c8ejsysg0skf4wr46x2fw-7sxsjqhmjmtd4pt8o4b8by3f5 172.31.39.40:2377
